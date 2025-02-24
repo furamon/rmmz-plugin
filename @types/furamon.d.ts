73c09fc599f98e0d75517e98bdabc754eb03724a
furamon <furamon913@gmail.com>
2025-02-24 20:55:19 +0900

    仕切り直し

--- @types/furamon.d.ts
+++ @types/furamon.d.ts
@@ -2,26 +2,31 @@ declare interface BattleManager {
   rangeEx(action: Game_Action, targets: Game_Battler[]): Game_Battler[];
 }
 
-declare interface Game_Battler {
+declare interface Game_BattlerBase {
+  isDummyEnemy(): boolean;
+}
+
+declare interface Game_Battler extends Game_BattlerBase {
   stepBack(): Promise<void>;
+  // isActor(): this is Game_Actor;
+}
+
+declare interface Game_Actor extends Game_Battler {
+  lp: number;
   _lp: number;
   mlp: number;
 
   maxLPSet: () => void;
-  lp(lp: number, mlp: number): number;
   recoverLP: () => void;
   _regeneDeath: boolean;
 }
 
-declare interface BattleManager {
-  rangeEx(arg0: Game_Action, targets: Game_Battler[]): Game_Battler[];
-}
-
 declare interface Game_ActionResult {
   lpDamage: number;
+  _isHitConfirm: boolean;
 }
 
-declare interface Sprite_Damage {
+declare interface Sprite_Damage extends Sprite {
   _isRegenerationWait: boolean;
   _spriteBattler: Sprite_Battler;
   _diffX: number;
@@ -31,7 +36,11 @@ declare interface Sprite_Damage {
   _delay: number;
 }
 
-declare interface Sprite_Gauge {
+declare interface Sprite extends PIXI.Sprite {
+  setup: (target: Game_Actor, type: string) => void;
+}
+
+declare interface Sprite_Gauge extends Sprite {
   _lpColor1: string;
   _lpColor2: string;
   _lpTextColorMax: string;
@@ -41,4 +50,22 @@ declare interface Sprite_Gauge {
 
 declare interface TextManager {
   lpA: string;
+}
+
+declare interface Scene_Battle {
+  _enemyNameWindow: Window_EnemyName;
+}
+
+declare interface TraitObject {
+  meta: { [key: string]: any };
+}
+
+declare interface Skill {
+  meta: { [key: string]: any };
+}
+
+declare interface Trait {
+  code: number;
+  dataId: number;
+  value: number;
 }
